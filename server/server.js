159edc55e5f65538f851fdef5d58639a222a7f05
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2024-04-05 07:40:52 +0500

    vercel deployment test

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(cors());
+app.use(cors({origin: 'https://frontend-alpha-one-22.vercel.app'}))
 app.use(express.json())
 
 const userRoute = require('./routes/userRoute');
