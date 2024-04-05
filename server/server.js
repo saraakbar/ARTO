7f028e024a740d9f998a7bc9da258e49c8d1b594
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2024-04-05 08:13:47 +0500

    vercel deployment test

--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
-require('dotenv').config()
-
-const express = require('express')
-const { default: mongoose } = require('mongoose')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
-mongoose.connect(process.env.DATABASE_URL, {})
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
 
-app.use(cors())
-app.use(cors({origin: 'https://frontend-alpha-one-22.vercel.app/'}))
-app.use(express.json())
+app.use(express.json());
+app.use(cors({
+  origin: 'https://frontend-alpha-one-22.vercel.app',
+  credentials: true 
+}));
 
 const userRoute = require('./routes/userRoute');
 const productRoute = require('./routes/productRoute');
-
 app.use(userRoute);
 app.use(productRoute);
 
-app.listen(8000, () => console.log('Server Started'))
+app.listen(8000, () => console.log('Server Started'));
