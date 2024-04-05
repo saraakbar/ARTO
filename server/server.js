ab6143245eaa63066efc2a6de705d5701a3fa0e1
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2024-04-05 08:35:08 +0500

    test

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
 app.use(express.json());
+app.use(cors());
 app.use(cors({
   origin: 'https://frontend-alpha-one-22.vercel.app',
 }));
