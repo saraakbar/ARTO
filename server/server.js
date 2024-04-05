4f94800e1789775c83521cf60747de279a1551e6
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2024-04-05 09:20:27 +0500

    Revert "vercel deployment test"
    This reverts commit 3ab7f3b25687262f88f44b2a56c96c9d661d2a80.

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
-app.use(cors())
 app.use(cors({origin: 'https://frontend-alpha-one-22.vercel.app/'}))
 app.use(express.json())
 
