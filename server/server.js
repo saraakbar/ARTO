677a9f6fe4404030a779113ef80954f97b09808d
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2024-04-05 09:20:38 +0500

    Revert "vercel deployment test"
    This reverts commit 159edc55e5f65538f851fdef5d58639a222a7f05.

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(cors());
-app.use(cors({origin: 'https://frontend-alpha-one-22.vercel.app'}))
 app.use(express.json())
 
 const userRoute = require('./routes/userRoute');
