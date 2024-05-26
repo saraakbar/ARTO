6c40b41ad3bd725cad213dc333be55f7c6475754
Sara Ebrahim <74443322+saraakbar@users.noreply.github.com>
2024-05-26 20:52:24 +0500

    UI fixes

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router-dom";
-import { faSignOutAlt, faGear, faUser, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faSignOutAlt, faGear, faUser, faHeart, faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import "./Dropdown.css";
+
 const UserMenuDropdown = ({ uname }) => {
   const [isHovered, setIsHovered] = useState(false);
   const navigate = useNavigate();
@@ -11,17 +12,18 @@ const UserMenuDropdown = ({ uname }) => {
     <div className="mr-2 dropdown-container">
       <div className="button-container">
         <button
-          className={`${isHovered ? 'px-6' : ''} text-white font-bold hover:px-6 uppercase rounded outline-none focus:outline-none ease-linear transition-all duration-150`}
+          className={`${isHovered ? 'px-4' : ''} text-white font-bold hover:px-4 uppercase  outline-none focus:outline-none ease-linear transition-all duration-150`}
         >
           <FontAwesomeIcon icon={faUser} className="mr-2" />
           {uname}
+          <FontAwesomeIcon icon={faAngleDown} className="ml-1" />
         </button>
-
         <div
           className={`dropdown-content text-base bg-white rounded-lg shadow-lg py-2 px-4`}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            >
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <div className="h-0 my-2 border border-solid border-t-0 border-gray-700 opacity-25" />
           <button
             className="hover:text-white text-left text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent"
             onClick={(e) => {
